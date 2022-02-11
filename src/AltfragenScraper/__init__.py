004f5f912224f3f3adeed10202823dfad52a11d4
Sample Text <sadajwnedjq@nasdjkw.com>
2022-02-11 20:29:48 +0100

    init

---
+++ src/AltfragenScraper/__init__.py
@@ -0,0 +1,7 @@
+"""Altfragen Scraper
+By Andretsch Adrian andretscha00@univie.ac.at
+
+Sammelt Altfragen + Antworten in Online-Prüfungen automatisch, konvertiert sie zu einer PDF die an Studierende des nächsten Semesters geteilt werden kann"""
+
+__version__ = '0.1.0'
+from . import *
