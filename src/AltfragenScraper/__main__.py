2223924439e24c5511cd35c42c6bcc50abc34f24
Sample Text <sadajwnedjq@nasdjkw.com>
2022-02-14 19:24:09 +0100

    Added basic Selenium functionality

--- src/AltfragenScraper/__main__.py
+++ src/AltfragenScraper/__main__.py
@@ -1,28 +1,23 @@
 """
-
-
+TODO: Write introduction into Logbox, would be sick as hell. Only thing I'm worried about it is that
+users might be confused as to why the instructions suddenly disappear off the screen... Maybe I'll have
+to implement a more sophisticated version of print_data lol
+TODO: Create custom configuration for how answered/unanswered questions should be displayed in
 """
 
 import logging; logging.basicConfig(level=logging.INFO, format='%(asctime)s -  %(levelname)s - %(message)s', datefmt='%H:%M:%S')
+import traceback
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import filedialog
+from selenium.common.exceptions import WebDriverException
 import scraper
-
-data = {
-    'q_num_123456': {
-        'index': '1',
-        'question': 'When did the second world war end? (2)',
-        'answers': {
-            '1945': False,
-            '1955': False,
-            '1939': True,
-            '1946': True
-        }
-    }
-}
+from scraper import data
 
 class Interface(tk.Tk):
+    # Just a couple of strings whose exact states are important
+    STR_CLOSE_BROWSER = 'Browser schließen'
+    STR_OPEN_BROWSER = 'Browser öffnen'
     def __init__(self):
         """
         Initializes the Tkinter window.
@@ -62,15 +57,15 @@ class Interface(tk.Tk):
         ttk.Label(frm_browser, text='Wähle Browser'
             ).grid(row=0, column=0, sticky='w', pady=(3,0))
 
-        preferred_browser = tk.StringVar(frm_browser, 'firefox')
+        self.choice_browser = tk.StringVar(frm_browser, 'firefox')
 
-        ttk.Radiobutton(frm_browser, text='Firefox', variable=preferred_browser, value='firefox', command=self.enable_btn_browser, state='normal'
+        ttk.Radiobutton(frm_browser, text='Firefox', variable=self.choice_browser, value='firefox', state='normal'
             ).grid(row=1, column=0, pady=(3, 0), sticky='w')
-        ttk.Radiobutton(frm_browser, text='Chrome', variable=preferred_browser, value='chrome', command=self.enable_btn_browser
+        ttk.Radiobutton(frm_browser, text='Chrome', variable=self.choice_browser, value='chrome'
             ).grid(row=2, column=0, pady=(3, 0), sticky='w')
         
-        self.btn_browser_text = tk.StringVar(frm_browser, 'Browser starten')
-        self.btn_browser = ttk.Button(frm_browser, textvariable=self.btn_browser_text, state='disabled', command=placeholder)
+        self.btn_browser_text = tk.StringVar(frm_browser, self.STR_OPEN_BROWSER)
+        self.btn_browser = ttk.Button(frm_browser, textvariable=self.btn_browser_text, command=self.on_btn_browser_click)
         self.btn_browser.grid(row=3, rowspan=1, column=0, pady=3, sticky='ew')
         
         # Save Dialogue
@@ -78,11 +73,11 @@ class Interface(tk.Tk):
         frm_save.grid(row=1, column=0, padx=(10,0), pady=10, sticky='s')
 
         self.btn_save = ttk.Button(frm_save, text='Altfragen speichern', state='disabled', command=placeholder)
-        self.btn_save.grid(row=0, column=0, sticky='ew')
+        self.btn_save.grid(row=1, column=0, sticky='ew')
         
-        self.lbl_save_text = tk.StringVar(frm_save, "Keine Altfragen vorhanden")
+        self.lbl_save_text = tk.StringVar(frm_save, "Noch keine Fragen")
         ttk.Label(frm_save, textvariable=self.lbl_save_text, font=(None, 8), foreground='#484848'
-            ).grid(row=1, column=0, padx=5)
+            ).grid(row=0, column=0, padx=5)
 
         # Logbox Wrapper containing Label and Browser Status
         frm_logbox_wrapper = tk.Frame(self)
@@ -93,15 +88,18 @@ class Interface(tk.Tk):
         ttk.Label(frm_logbox_wrapper, text='Log/Altfragen'
             ).grid(row=0, column=0, sticky='w', pady=(0, 3))
         
-        self.lbl_browser_status_text = tk.StringVar(frm_logbox_wrapper, "Browser Status: Lorem Ipsum Dolor Sit Am")
+        self.lbl_browser_status_text = tk.StringVar(frm_logbox_wrapper)
         ttk.Label(frm_logbox_wrapper, textvariable=self.lbl_browser_status_text, foreground=('#505050')
             ).grid(row=0, column=1, padx=20, pady=(0,5), sticky='e')
+        self.set_browser_status('Nicht gestartet')
 
         # Logbox itself
         frm_logbox = tk.Frame(frm_logbox_wrapper)
         frm_logbox.grid(row=1, column=0, columnspan=2, sticky='nsew')
 
-        self.logbox = tk.Text(frm_logbox, state='disabled', wrap='word')
+        ttk.Style().configure('TText', padding='5 1 1 1')
+
+        self.logbox = tk.Text(frm_logbox, state='disabled', wrap='word', font=('Helvetica', 10), borderwidth=10, relief=tk.FLAT)  # Border creates illusion of padding inside text box
         self.logbox.grid(row=1, sticky='nsew')
 
         logbox_scrollbar = ttk.Scrollbar(frm_logbox, orient='vertical', command=self.logbox.yview)
@@ -146,13 +144,51 @@ class Interface(tk.Tk):
 
     def log(self, string):
         """Sends a string to self.txt_log; important to note: self.print_data() will overwrite what's in log every time it's run"""
+        self.logbox['state'] = 'normal'
         self.logbox.insert('end', ('' if self.logbox.get(0.0, 'end') else '\n') + string)
         self.logbox.yview_moveto(5000.0)  # 'end' doesn't work here for some reason, so I just went for a really high number.
+        self.logbox['state'] = 'disabled'
 
-    
-    def enable_btn_browser(self):
-        """Ungrays the Browser Starten button upon selecting a browser with the radio buttons"""
-        self.btn_browser['state'] = 'normal'
+
+    def on_btn_browser_click(self):
+        """Figures out which state the browser is in and starts the relevant processes"""
+        if self.btn_browser_text.get() == self.STR_OPEN_BROWSER:
+            self.selenium_run()
+            self.btn_browser_text.set(self.STR_CLOSE_BROWSER)
+
+        elif self.btn_browser_text.get() == self.STR_CLOSE_BROWSER:
+            self.driver.quit()
+            self.btn_browser_text.set(self.STR_OPEN_BROWSER)
+
+
+    def selenium_run(self):
+        """Tries to start Selenium and runs the scraper"""
+        try:
+            self.set_browser_status('Startet...')
+            self.driver = scraper.get_selenium(self.choice_browser.get())
+            self.selenium_listener()
+        except Exception:
+            str_traceback = traceback.format_exc()
+            logging.critical('Couldn\'t open webdriver:\n' + str_traceback)
+            self.set_browser_status('Fehler')
+            self.log('\n\n' + str_traceback)
+
+        scraper.scrape(self.driver, self)
+
+
+    def selenium_listener(self):
+        """Periodically checks that Selenium is still alive. If it died, it changes the text on self.btn_browser_text to 'Browser Schließen'"""
+        try: 
+            self.driver.current_url  # Throws up an exception when browser is closed
+            self.after(250, self.selenium_listener)  # Function recursively calls itself
+        except WebDriverException:
+            self.set_browser_status('Unerwartet geschlossen')
+            self.btn_browser_text.set(self.STR_OPEN_BROWSER)
+
+
+    def set_browser_status(self, string):
+        self.lbl_browser_status_text.set('Browser Status: ' + string)
+        logging.debug('Browser Status: ' + string)
 
 
 def placeholder():
@@ -162,9 +198,84 @@ def placeholder():
 
 def main():
     root = Interface()
-    root.print_data()
-    root.mainloop()
+    root.log("""1. Wie heißt die Logik des Forschungsprozesses? (1)
+a. Methodologie
+b. Prozessologie
+c.*Epistemologie
+d. Pragmatologie
 
+2. Was trifft zu? (3)
+Laut Émile Durkheim lassen sich soziale Tatbestände durch psychologische Faktoren erklären.
+WEIL
+Émile Durkheim wollte der Ethnographie zum Durchbruch verhelfen.
+a. Die erste Aussage ist richtig.
+b.*Die erste Aussage ist falsch.
+c.*Die zweite Aussage ist richtig.
+d. Die zweite Aussage ist falsch.
+e. Die Weil-Verknüpfung ist berechtigt.
+f.*Die Weil-Verknüpfung ist falsch.
+
+3. Was trifft zu? (3)
+Die Studie „Die Arbeitslosen von Marienthal“ lässt sich als Aktionsforschung bezeichnen.
+WEIL
+Die Forschenden der Studie „Die Arbeitslosen von Marienthal“ sollten die Rolle eins für die Gemeinschaft nützlichen
+Mitglieds einnehmen.
+a.*Die erste Aussage ist richtig.
+b. Die erste Aussage ist falsch.
+c.*Die zweite Aussage ist richtig.
+d. Die zweite Aussage ist falsch.
+e.*Die Weil-Verknüpfung ist berechtigt.
+f. Die Weil-Verknüpfung ist falsch.
+
+4. Wie ist Émile Durkheim in seiner Studie „Der Selbstmord“ vorgegangen? (1)
+a. Er hat experimentelle und natürliche Daten kombiniert.
+b. Er hat psychologische Tests durchgeführt.
+c. Er hat persönliche Dokumente analysiert.
+d.*Er hat Hypothesen aufgestellt und überprüft.
+
+5. Welche der folgenden Merkmale stehen für eine erklärende Vorgehensweise in den Sozialwissenschaften? (2)
+a.*hypothesentestend
+b.*quantitativ
+c. situationsbezogen
+d. theoriegenerierend
+e. qualitativ
+
+6. Das Zusammenfassen mehrerer Variablen zu einer Kennzahl nennt man... (1)
+a. Panel.
+b. Kausalzusammenhang.
+c. Normalverteilung.
+d.*Index.
+
+7. Welcher Erkenntnislogik folgt William F. Whytes Studie "Street Corner Society"? (2)
+a. deduktiv
+b.*idiographisch
+c. kausal-erklärend
+d. nomothetisch
+e.*qualitativ
+
+8. Unter Kausalität versteht man... (1)
+a. das Zusammenfassen von mindestens zwei Variablen.
+b. das gleichzeitige Auftreten von zwei Phänomenen.
+c. die Schlussfolgerung aus einer logischen Ableitung.
+d.*einen bekannten Ursache-Wirkung Zusammenhang.
+
+9. Ein Beispiel:
+In einem oberösterreichischen Badeort wird die See nahe Landschaft zunehmend durch Zweitwohnsitze verbaut. Um
+die daraus entstehenden Konflikte zu untersuchen, wurde ein Schwimmwettbewerb zwischen der örtlichen
+Bevölkerung und Personen mit Zweitwohnsitz organisiert und das Verhalten beider Gruppen beobachtet.
+Um welchen Typus von Daten würde es sich nach den heuristischen Achsen von Lazarsfeld handeln? (1)
+a.*experimentell
+b. komplex
+c. subjektiv
+d. natürlich
+
+10. Schlüsselpersonen spielen eine wichtige Rolle in der Feldforschung, weil sie...(1)
+a. die Forschung leiten.
+b.*den Zugang zum Feld verschaffen.
+c. als Testpersonen fungieren.
+d. Forschungsprojekte finanzieren.""")
+    root.mainloop()
+    
 
 if __name__ == '__main__':
     main()
--- src/AltfragenScraper/scraper.py
+++ src/AltfragenScraper/scraper.py
@@ -1,2 +1,41 @@
-def scraper():
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from subprocess import CREATE_NO_WINDOW
+
+data = {
+    'q_num_123456': {
+        'index': '1',
+        'question': 'When did the second world war end in full commitment as according to the Paris Climate Accord of 1923? (2)',
+        'answers': {
+            '1945': False,
+            '1955': False,
+            '1939': True,
+            '1946': True
+        },
+        'answer_order': ['1945', '1955', '1939', '1946']
+    }
+}
+
+def get_selenium(browser: str):
+    """Starts Selenium without console window """
+    try:
+        if browser == 'chrome':
+            chrome_service = ChromeService('chromedriver')
+            chrome_service.creationflags = CREATE_NO_WINDOW
+            return webdriver.Chrome()
+
+        elif browser == 'firefox':
+            firefox_service = FirefoxService('geckodriver')
+            firefox_service.creationflags = CREATE_NO_WINDOW
+            return webdriver.Firefox()
+
+        else:
+            raise ValueError("str must read 'chrome' or 'firefox', not " + browser)
+    except:
+        raise Exception('Something with Selenium went wrong')
+
+
+def scrape(driver, interface, data=data):
+    inter = interface
     pass
