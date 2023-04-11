19ad31aeaa4d5a2396f03a86393089c5c87069c5
Emile-Durkheim <98767867+Emile-Durkheim@users.noreply.github.com>
2023-04-11 15:04:32 +0200

    remove sample data, add some documentation

--- src/AltfragenScraper/_data.py
+++ src/AltfragenScraper/_data.py
@@ -1,6 +1,16 @@
+""" 
+Questions are stored in the below data object, which is referenced at different points in the program.
+
+The structure looks about as follows: 
 data = {
-    'sample': {'question': 'hello world',
+    'id_4837': {'question': 'hello world',
                 'answers': ['what', 'is', 'up', 'my', 'dudes'],
                 'is_selected': [False, False, True, False, True]},
-    'question_order': ['sample']
+    'id_5131': {'question': 'hello world',
+                'answers': ['what', 'is', 'up', 'my', 'dudes'],
+                'is_selected': [False, False, True, False, True]},
+    'question_order': ['id_4837', 'id_5131']
 }
+"""
+
+data = {}
